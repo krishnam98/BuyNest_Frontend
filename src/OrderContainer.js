d140855aaf84fb7331e4ed704fb78b9417867149
krishnam98 <sonikrishnam98@gmail.com>
2026-01-04 17:50:59 +0530

    fixed bug in order section

--- src/OrderContainer.js
+++ src/OrderContainer.js
@@ -9,7 +9,7 @@ function OrderContainer({ info }) {
 
     const date = new Date(timeStamp);
     const day = String(date.getUTCDate()).padStart(2, "0");
-    const month = String(date.getUTCMonth()).padStart(2, "0");
+    const month = String(date.getUTCMonth() + 1).padStart(2, "0");
     const year = date.getUTCFullYear();
 
     const formattedDate = `${day}-${month}-${year}`;
